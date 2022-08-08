412e113b48c0a539093e44f1bc35610d425ec89b
aguilanbon <aguilanbon@gmail.com>
2022-08-08 18:34:47 +0800

    footer color

--- src/pages/Includes/Footer.jsx
+++ src/pages/Includes/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
     return (
         <div className='w-full flex items-center justify-center p-2'>
-            <h3 className='text-xs font-semibold'>Bon Aguilan | Front End Developer</h3>
+            <h3 className='text-xs font-semibold text-gray-400'>Bon Aguilan | Front End Developer</h3>
         </div>
     )
 }
