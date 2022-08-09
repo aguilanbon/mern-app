506bef3cdf1ef893f7e54eb3f822beff48c84ab6
aguilanbon <aguilanbon@gmail.com>
2022-08-09 15:54:54 +0800

    sidebar navigation component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 function Home() {
     return (
         <div className='w-full min-h-screen flex'>
-            <h1>Home</h1>
+            <Navigation />
         </div>
     )
 }
---
+++ src/pages/Home/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div className='xl:w-60 bg-primary'>
+        <div className='w-full flex-flex-col'>
+            <div className='w-full h-12 bg-bg flex items-center justify-center'>
+                <h1 className='text-primary font-bold'>Profiling App</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navigation
