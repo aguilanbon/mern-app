bdbca324ca89271ca64eef9ade09c90e9327f079
aguilanbon <aguilanbon@gmail.com>
2022-08-15 11:24:24 +0800

    screen w below 768 collapsed nav by default

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,10 +4,24 @@ import Dashboard from './Dashboard'
 import Navigation from './Navigation'
 import TopBar from './TopBar'
 import GlobalHelperContext from '../../helpers/GlobalHelperContext'
+import { useEffect } from 'react'
+import { useState } from 'react'
 
 function Home() {
 
-    const { sideNavOpen } = useContext(GlobalHelperContext)
+    const { sideNavOpen, setSideNavOpen } = useContext(GlobalHelperContext)
+
+    const [width, setWidth] = useState(window.innerWidth)
+
+    const updateWidth = () => {
+        setWidth(window.innerWidth)
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', updateWidth)
+
+        if(width < 768) setSideNavOpen(false)
+    },[])
 
     return (
         <div className='w-full min-h-screen flex'>
