4e89c880a23b8522a83dd83e959d30442b592d41
aguilanbon <aguilanbon@gmail.com>
2022-09-03 19:51:53 +0800

    color change client card

--- src/pages/Home/ClientsCard.jsx
+++ src/pages/Home/ClientsCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function ClientsCard() {
   return (
     <div className='w-full h-auto'>
-        <div className='w-full flex items-center rounded-sm border bg-hoversecondary text-primary h-14 p-2 text-sm'>
+        <div className='w-full flex items-center rounded-sm border bg-slate-300 text-primary h-14 p-2 text-sm'>
             <div className='flex mr-8'>
                 <img src="./aloy.jpg" alt="" className='w-12 h-12 rounded-full object-cover' />
             </div>
