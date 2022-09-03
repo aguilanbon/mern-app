7f885a681985b465ca31de4fa77d744b873928d8
aguilanbon <aguilanbon@gmail.com>
2022-09-03 19:47:20 +0800

    initial layout for client cards

--- src/pages/Home/ClientsContainer.jsx
+++ src/pages/Home/ClientsContainer.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import ClientsCard from './ClientsCard'
 
 function ClientsContainer() {
   return (
-    <div className='w-full h-auto'>
-        <div>
+    <div className='w-1/2 h-auto'>
+        <div className='mb-4'>
             <h1 className='font-bold text-lg opacity-80'>Clients Overview</h1>
         </div>
         <div>
-
+            <ClientsCard />
+            <ClientsCard />
+            <ClientsCard />
+            <ClientsCard />
+            <ClientsCard />
+            <ClientsCard />
+            <ClientsCard />
         </div>
     </div>
   )
