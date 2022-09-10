26822802d92bb7290d13db0b1282dd53e8b2758b
aguilanbon <aguilanbon@gmail.com>
2022-09-10 19:30:25 +0800

    hid address on smaller devices

--- src/pages/Home/ClientsCard.jsx
+++ src/pages/Home/ClientsCard.jsx
@@ -4,17 +4,17 @@ function ClientsCard() {
   return (
     <div className='w-full h-auto'>
         <div className='w-full flex items-center rounded-sm border bg-slate-300 text-primary h-14 p-2 text-sm'>
-            <div className='flex mr-8'>
+            <div className='flex md:mr-8 mr-4'>
                 <img src="./aloy.jpg" alt="" className='w-12 h-12 rounded-full object-cover' />
             </div>
-            <div className='flex w-1/4'>
+            <div className='flex md:w-1/4 w-1/3'>
                 <h2 className='mr-1'>Aloy</h2>
                 <h2>Zero</h2>
             </div>
-            <div className='flex w-1/4'>
+            <div className='flex md:w-1/4 w-1/3'>
                 <h2>Admin Assistant</h2>
             </div>
-            <div className='flex w-1/4'>
+            <div className='md:flex hidden w-1/4'>
                 <h2>Avocado St. Narra, Palawan</h2>
             </div>
         </div>
